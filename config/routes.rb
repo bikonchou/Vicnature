52db0c9b06e258e6796893696fbb0f6968d61de8
bikonchou <bikon0102@gmail.com>
2015-06-04 16:47:55 +0800

    fix new-user form error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'welcome#index'
   resources :welcome  
+  
   resources :categories do
     resources :products, :controller => 'category_products'
   end
